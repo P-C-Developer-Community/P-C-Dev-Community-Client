767b5614d400ab5b6f2e765238d5727567726257
Christoph Weber <WeberChristoph85@gmail.com>
2023-04-13 16:03:30 +0200

    bug fixes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,15 +2,13 @@ import { Link } from "react-router-dom";
 
 // We are deconstructing props object directly in the parentheses of the function
 function ProjectCard ( { title, description, _id, owner } ) {
-  
-  console.log("owner.name///////", owner.name)
 
   return (
     <div className="ProjectCard card">
       <Link to={`/projects/${_id}`}>
-        <h3>{title}</h3>
+        <h3>Title {title}</h3>
       </Link>
-      <h2>Owner {owner.name}</h2>
+      {/* <h2>Owner {owner.name}</h2> */}
       <p style={{ maxWidth: "400px" }}>{description} </p>
     </div>
   );
