fec242376672c7ba9626adaa36ce6c4f8b6b2ed0
Christoph Weber <WeberChristoph85@gmail.com>
2023-04-14 09:22:54 +0200

    updated contribution card

--- src/components/ContributionCard.js
+++ src/components/ContributionCard.js
@@ -6,7 +6,7 @@ function ContributionCard({ title, description, _id }) {
       <div className="bg-slate-400 rounded-lg shadow-xl shadow-cyan-500/40 hover:shadow-slate-50 relative">
       <Link to={`/contributions/${_id}`}>
         <div className="p-4">
-        <div className="absolute  h-8 w-8 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${logo})` }}></div>
+        <div className="absolute  h-8 w-8 bg-cover bg-no-repeat bg-center" ></div>
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         <p className="text-gray-700 text-base mb-4">{description} </p>
         <div className="flex justify-between items-center" >
