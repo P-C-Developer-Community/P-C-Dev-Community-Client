4d2410580dc2b29f4e70e922299a83d70f477fa5
Christoph Weber <WeberChristoph85@gmail.com>
2023-04-19 15:34:15 +0200

    style changes

--- src/pages/CommunityPage.js
+++ src/pages/CommunityPage.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { AuthContext } from "../context/auth.context";
 import { useContext, useState, useEffect } from "react";
-import backgroundImage from "../assets/network.jpeg"
+import backgroundImage from "../assets/network.jpeg";
+import GitHub from "../assets/Icons/gitHub.png";
+
+
 
 function CommunityPage() {
   const [users, setUsers] = useState([]);
@@ -75,55 +78,70 @@ function CommunityPage() {
       </h1>
 
       <div className=" rounded-t-lg mb-4">
-        <div className="p-4">
-          <div className="grid grid-cols-4 sm:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-10"></div>
+        <div className="p-4 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-16 gap-y-10">
           {users.map((user, index) => {
             return (
-              <div key={index} className="bg-transparent border rounded-xl shadow-md p-4">
+              <div
+                key={index}
+                className="backdrop-blur-sm border grid justify-items-center text-white rounded-xl shadow-md p-4">
                 <img
-                  className="mx-8 my-6 h-24 w-24 rounded-4xl"
+                  className=" h-24 w-24  rounded-3xl"
                   src={user.imageUrl}
                   alt=""
                   srcset=""
                 />
-                <label className="text-2xl font-bold text-gray-800">{users.name}</label>
-                <p className="text-gray-600" >Email: {user.email}</p>
-                <p className="text-gray-600">
+                <p className="text-2xl font-bold text-white">{user.name}</p>
+                <p className="text-slate-300 -mt-1">@: {user.email}</p>
+                <p className="text-slate-300 mt-1">
                   Member Since:{" "}
                   {new Date(user.createdAt).toLocaleDateString("en-US", {
-                    weekday: "short",
-                    month: "long",
+                    
+                    month: "short",
                     day: "numeric",
                     year: "numeric",
                   })}
                 </p>
                 <div className="mt-4">
-
-                  {user.gitHub && <p className="">Github: {user.gitHub}</p>}
-            {user.linkedIn && <p className="">LinkedIn: {user.linkedIn}</p>}
-            {user.twitter && <p className="">Twitter: {user.twitter}</p>}
-            {user.instagram && <p className="">Instagram: {user.instagram}</p>}
-            {counts[user._id] && (
-              <>
-                <p>Number of Active Projects: {counts[user._id].projects}</p>
-                <p>
-                  Number of Active Contributions:{" "}
-                  {counts[user._id].contributions}
-                </p>
-              </>
-              )}
+                  {user.gitHub && (
+                    <div className="flex items-center">   
+                    <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16"> <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" fill="white"></path> </svg> 
+                    
+                    {user.gitHub}
+                    
+                    </div>
+                    )}
+                 
+                 {user.linkedIn && (
+                    <p className="">LinkedIn: {user.linkedIn}</p>
+                  )}
+                 
+                  {user.twitter && <p className="">Twitter: {user.twitter}</p>}
+                 
+                  {user.instagram && (
+                    <p className="">Instagram: {user.instagram}</p>
+                  )}
+                 
+                  {counts[user._id] && (
+                    <>
+                      <p>
+                        Active Projects: {counts[user._id].projects}
+                      </p>
+                      <p>
+                        Active Collaborations:{" "}
+                        {counts[user._id].contributions}
+                      </p>
+                    </>
+                  )}
                 </div>
               </div>
-      
             );
           })}
-          </div>
+        </div>
       </div>
     </div>
-  
-    
+    </div>
   );
 }
 
 export default CommunityPage;
-
